5236f0fa3325f8f619014347571437488927bd28
windspring <sagan.pawel1000@gmail.com>
2020-05-04 20:01:21 +0200

    removing comments

--- counter.vhd
+++ counter.vhd
@@ -1,42 +1,13 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    09:05:25 04/05/2020 
--- Design Name: 
--- Module Name:    counter - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity counter is
     Port (  clk : in  STD_LOGIC;
 				reset : in  STD_LOGIC;
-<<<<<<< HEAD
             ticks : out  STD_LOGIC_VECTOR (15 downto 0));
-=======
-            ticks : out  STD_LOGIC_VECTOR (7 downto 0));
->>>>>>> e46fe4a... adding simple counter mod 256 impl
 end counter;
 
 architecture Behavioral of counter is
