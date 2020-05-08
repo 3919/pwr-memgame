e831b7ab2eae29e0ea970cf77068f030bcfe6ed6
SecMeant <secmeant@gmail.com>
2020-05-09 00:21:44 +0200

    Fixing synthesis bug (module not allowed with non const values)

--- object_generator.vhd
+++ object_generator.vhd
@@ -12,39 +12,42 @@ entity object_generator is
     clock          : in   std_logic;
     i_object_count : in   STD_LOGIC_VECTOR (15 downto 0);
     i_reset        : in   STD_LOGIC;
-   o_ready        : out  std_logic;
-   i_random       : in   STD_LOGIC_VECTOR (7 downto 0);
+    o_ready        : out  std_logic;
+    i_random       : in   STD_LOGIC_VECTOR (7 downto 0);
     o_mem_we       : out  STD_LOGIC;
     o_mem_address  : out  STD_LOGIC_VECTOR (15 downto 0);
-   i_mem_datain   : in   STD_LOGIC_VECTOR (7 downto 0);
+    i_mem_datain   : in   STD_LOGIC_VECTOR (7 downto 0);
     o_mem_dataout  : out  STD_LOGIC_VECTOR (7 downto 0)
   );
 end object_generator;
 
-architecture Behavioral of object_generator is
-  type state_type is (WAITING, GENERATING_PREPARE, GENERATING_SHUFFLE, GENERATING_SWAP_RD1, GENERATING_SWAP_RD2, GENERATING_SWAP_WR, READY);
+larchitecture Behavioral of object_generator is
+  type state_type is (RESET, GENERATING_PREPARE, GENERATING_SHUFFLE, GENERATING_SWAP_RD1, GENERATING_SWAP_RD2, GENERATING_SWAP_WR, READY);
   signal state : state_type;
   signal current_address : std_logic_vector(o_mem_address'range);
   signal swap_buffer_current : std_logic_vector(i_mem_datain'range);
   signal swap_buffer_random : std_logic_vector(i_mem_datain'range);
+  signal random_address : unsigned(o_mem_address'range);
+
+  constant MAX_OBJ : integer := 960;
 begin
 
   GenerateProc: process(clock, i_reset)
-    variable random_address : unsigned(o_mem_address'range);
     variable counter : unsigned(o_mem_dataout'range);
   begin
     if i_reset = '1' then
-      state <= WAITING;
-      current_address <= (others => '0');
-      random_address := (unsigned(i_random) mod 10) + 48;
+      state <= RESET;
+
     elsif rising_edge(clock) then
       case state is
-        when WAITING =>
+        when RESET =>
           state <= GENERATING_PREPARE;
-          counter := (others => '0');
-          o_ready <= '1';
-        when GENERATING_PREPARE =>
+          current_address <= (others => '0');
+          random_address <= X"00" & (unsigned(i_random) mod MAX_OBJ);
+          counter := X"48";
           o_ready <= '0';
+
+        when GENERATING_PREPARE =>
           if current_address /= i_object_count then
             o_mem_we <= '1';
             o_mem_address <= current_address;
@@ -56,6 +59,7 @@ begin
             current_address <= std_logic_vector(to_unsigned(1,current_address'length));
             state <= GENERATING_SHUFFLE;
           end if;
+
         when GENERATING_SHUFFLE =>
           o_mem_we <= '0';
           if current_address /= i_object_count then
@@ -65,16 +69,19 @@ begin
           else
             state <= READY;
           end if;
+
         when GENERATING_SWAP_RD1 =>
           -- Read from current address. Prepared in previous state.
           swap_buffer_current <= i_mem_datain;
 
           -- Prepare for next cycle (2nd read, random address smaller than current)
-          random_address := (unsigned(i_random) mod unsigned(current_address));
+          -- Random address is prepared in previous state with iterative method.
+          random_address <= X"00" & (unsigned(i_random) mod MAX_OBJ);
           o_mem_we <= '0';
           o_mem_address <= std_logic_vector(random_address);
 
           state <= GENERATING_SWAP_RD2;
+
         when GENERATING_SWAP_RD2 =>
           swap_buffer_random <= i_mem_datain;
 
@@ -83,6 +90,7 @@ begin
           o_mem_dataout <= swap_buffer_current;
 
           state <= GENERATING_SWAP_WR;
+
         when GENERATING_SWAP_WR =>
           -- Prepare for next cycle (write at current address, cached value from random address)
           o_mem_we <= '1';
@@ -92,8 +100,10 @@ begin
           current_address <= std_logic_vector(unsigned(current_address) + 1);
 
           state <= GENERATING_SHUFFLE;
+
         when READY =>
           o_ready <= '1';
+
       end case;
     end if;
   end process GenerateProc;
