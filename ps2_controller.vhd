9964985a74626ee9b67511fee5c327a0e27ee2dc
windspring <sagan.pawel1000@gmail.com>
2020-05-09 15:12:53 +0200

    ps2 controller synthesize pass, vga synthesize failed

--- ps2_controller.vhd
+++ ps2_controller.vhd
@@ -4,80 +4,90 @@ use IEEE.NUMERIC_STD.ALL;
 use IEEE.STD_LOGIC_SIGNED.ALL;
 
 entity ps2_controller is
-    Port (clk : in  STD_LOGIC;
-		  B1_status : in  STD_LOGIC_VECTOR (7 downto 0);
-		  B2_X : in  STD_LOGIC_VECTOR (7 downto 0);
-		  B3_Y : in  STD_LOGIC_VECTOR (7 downto 0);
-		  dataready : in STD_LOGIC;
-		  mouse_irq : out STD_LOGIC;
-		  logic_irq : in STD_LOGIC;
-		  x_cur_pos : out STD_LOGIC_VECTOR (15 downto 0);
-		  y_cur_pos : out STD_LOGIC_VECTOR (15 downto 0));
+    Port (B1_status : in  STD_LOGIC_VECTOR (7 downto 0); -- button status, signes etc. return by mouse
+		B2_X : in  STD_LOGIC_VECTOR (7 downto 0); -- x offset returned by mouse
+		B3_Y : in  STD_LOGIC_VECTOR (7 downto 0); -- y offset returned by mouse
+		dataready : in STD_LOGIC;  -- signal send by mouse when some new data is ready
+		mouse_irq : out STD_LOGIC; -- inform logic that user clicked button
+		logic_irq : in STD_LOGIC; -- get information if data was read
+		mouse_x : out STD_LOGIC_VECTOR (15 downto 0);
+		mouse_y : out STD_LOGIC_VECTOR (15 downto 0));
 end ps2_controller;
 
 architecture Behavioral of ps2_controller is
-	 
+	constant max_x_pos : integer := 4800;
+	constant max_y_pos : integer := 2000;
 	-- general signals 
-	signal clk_s : STD_LOGIC;
 	signal mouse_irq_s : STD_LOGIC := '0';
-	signal cur_pos_x_s : STD_LOGIC_VECTOR (15 downto 0) := (others => '0');
-	signal cur_pos_y_s : STD_LOGIC_VECTOR (15 downto 0) := (others => '0');
+	signal mouse_x_s : STD_LOGIC_VECTOR (15 downto 0) := (others => '0');
+	signal mouse_y_s : STD_LOGIC_VECTOR (15 downto 0) := (others => '0');
 
-	-- register signals
-	signal en_reg : STD_LOGIC;
 	-- reg with x position signals
-	signal data_in_reg_x : STD_LOGIC_VECTOR (15 downto 0):= (others => '0');
+	signal mouse_raw_x : STD_LOGIC_VECTOR (15 downto 0):= (others => '0');
 	-- reg with y position signals
-	signal data_in_reg_y : STD_LOGIC_VECTOR (15 downto 0):= (others => '0');
+	signal mouse_raw_y : STD_LOGIC_VECTOR (15 downto 0):= (others => '0');
   
 begin
-	clk_s <= clk;
+
 	mouse_irq <= mouse_irq_s;
 
-calc_pos:process(data_in_reg_x, data_in_reg_y)
-begin 
-		cur_pos_x_s <= cur_pos_x_s;
-		cur_pos_y_s <= cur_pos_y_s;
+calc_pos:process(mouse_raw_x, mouse_raw_y, B1_status)
+begin
 		
-		if B1_status(4) = '0' and cur_pos_x_s < 4800 then
-		  cur_pos_x_s <= cur_pos_x_s + data_in_reg_x;
-		elsif cur_pos_x_s > 0 then
-		  cur_pos_x_s <= cur_pos_x_s - data_in_reg_x;
-		end if;
-	
-		if B1_status(5) = '0' and cur_pos_y_s < 2000 then
-		  cur_pos_y_s <= cur_pos_y_s + data_in_reg_y;
-		elsif cur_pos_y_s > 0 then
-		  cur_pos_y_s <= cur_pos_y_s - data_in_reg_y;
-		end if;
+	if B1_status(4) = '0' then
+	  mouse_x_s <= mouse_x_s + mouse_raw_x;
+	else
+	  mouse_x_s <= (mouse_x_s - mouse_raw_x);
+	end if;
+
+	if B1_status(5) = '0' then
+	  mouse_y_s <= (mouse_y_s + mouse_raw_y);
+	else
+	  mouse_y_s <= (mouse_y_s - mouse_raw_y);
+	end if;
+
+	if mouse_x_s >= max_x_pos then
+		mouse_x_s <= X"12c0";
+	end if;
+
+	if mouse_x_s < 0 then
+		mouse_x_s <= X"0000";
+	end if;
+
+	if mouse_y_s >= max_y_pos then
+		mouse_y_s <= X"07d0";
+	end if;
+
+	if mouse_y_s < 0 then
+		mouse_y_s <= X"0000";
+	end if;
 		
 end process;
 
-assign_pos:process(cur_pos_x_s, cur_pos_y_s)
+assign_pos:process(mouse_x_s, mouse_y_s, logic_irq)
 begin
 	if logic_irq ='0' then
-		if cur_pos_x_s < 0  then
-			x_cur_pos <= X"0000";
-		elsif cur_pos_x_s >= 4800 then
-			x_cur_pos <= X"12c0";
+		if mouse_x_s < 0  then
+			mouse_x <= X"0000";
+		elsif mouse_x_s >= max_x_pos then
+			mouse_x <= X"12c0";
 		else
-			x_cur_pos <= cur_pos_x_s;
+			mouse_x <= mouse_x_s;
 		end if;
-		
-		if cur_pos_y_s < 0 then
-			y_cur_pos <= X"0000";
-		elsif cur_pos_y_s >= 2000 then
-			y_cur_pos <= X"07d0";
+
+		if mouse_y_s < 0 then
+			mouse_y <= X"0000";
+		elsif mouse_y_s >= max_y_pos then
+			mouse_y <= X"07d0";
 		else
-			y_cur_pos <= cur_pos_y_s;
+			mouse_y <= mouse_y_s;
 		end if;
 	end if;
 end process;
 
-data_in_reg_x <= x"00"&B2_X when dataready = '1';
-data_in_reg_y <= x"00"&B3_Y when dataready = '1';
+mouse_raw_x <= x"00"&B2_X when dataready = '1' else x"0000";
+mouse_raw_y <= x"00"&B3_Y when dataready = '1' else x"0000";
 
 mouse_irq_s <= '1' when B1_status(0) ='1' or logic_irq = '1' else '0';
-
 
 end Behavioral;
--- vga_driver.vhd
+++ vga_driver.vhd
@@ -9,9 +9,10 @@ entity vga_driver is
 				work	      : out    STD_LOGIC;
 				mem_address : out    std_logic_vector(15 downto 0);
 				mem_datain  : in     std_logic_vector(7 downto 0);
-				ps2_pos		: in     std_logic_vector(15 downto 0);
+				mouse_pos	: in     std_logic_vector(15 downto 0);
 				vga_data    : out    std_logic_vector(7 downto 0);
 				vga_we		: out 	std_logic;
+				vga_reset	: out 	std_logic;
 				vga_busy		: in 	   std_logic);
 end vga_driver;
 
@@ -22,63 +23,83 @@ architecture Behavioral of vga_driver is
  signal ticks : integer range 0 to 1024 := 0;
  
  type char_table is array(natural range <>) of std_logic_vector(7 downto 0);
- constant message_lost : char_table(0 to 7):= (X"59",X"6f",X"75",X"20",X"6c",X"6f",X"73",X"74"); -- You lost
+ constant message_lost : char_table(7 downto 0):= (X"59",X"6f",X"75",X"20",X"6c",X"6f",X"73",X"74"); -- You lost
  constant message_win : char_table(6 downto 0):= (X"59",X"6f",X"75",X"20",X"77",X"69",X"6e");  -- You win
  
  constant black_tail : std_logic_vector(7 downto 0) := X"10";
  constant cursor : std_logic_vector(7 downto 0) := X"1e";
  constant space : std_logic_vector(7 downto 0) := X"20";
  
+ -- VGA signals
+ signal vga_we_s		: std_logic :='0';
+ signal vga_reset_s	: std_logic :='0';
+ 
 begin
-
 	mem_address <= std_logic_vector(to_unsigned(ticks, mem_address'LENGTH));
-	work <= '1' when (enable ='1' and ticks < display_size) else '0'; 
-
-	load_img: process(ticks, enable)
+	vga_we <= vga_we_s;
+	vga_reset <= vga_reset_s;
+	
+	vga_reset_s <= '0' when enable ='1' else '1';
+	
+	load_img: process(ticks, enable, mode, mem_datain)
 	begin
-		if enable = '1' then
-			if mode = "00" then -- start mode, when everything is render as it is
-				if ticks = to_integer(unsigned(ps2_pos)) then
+		
+		if enable = '1'
+		then
+			if mode = "00" 
+			then -- start mode, when everything is read as it is
+				if ticks = to_integer(unsigned(mouse_pos)) 
+				then
 					vga_data <= cursor;
 				else
 					vga_data <= mem_datain;
 				end if;
-			elsif mode = "01" then -- hidden mode, each number is converted to black tail
-				if ticks = to_integer(unsigned(ps2_pos)) then
+			elsif mode = "01" 
+			then -- hidden mode, each number is converted to black tail
+				if ticks = to_integer(unsigned(mouse_pos)) 
+				then
 					vga_data <= cursor;
 				else
-					if mem_datain /= space then
+					if mem_datain /= space 
+					then
 						vga_data <= black_tail; 
 					else
 						vga_data <= space;
 					end if;
 				end if;
-			elsif mode = "10" then -- user lost mode, print communicate that user lost
-					if ticks < message_lost'LENGTH then
+			elsif mode = "10" 
+			then -- user lost mode, print communicate that user lost
+					if ticks < message_lost'LENGTH 
+					then
 						vga_data <= message_lost(ticks);
 					else
 						vga_data <= space;
 					end if;
 			else -- user win mode, print communicate that user win
-				if ticks < message_win'LENGTH then
+				if ticks < message_win'LENGTH 
+				then
 						vga_data <= message_win(ticks);
 					else
 						vga_data <= space;
 					end if;
 			end if;
+		else
+			vga_data <= X"00";
 		end if;
 	end process;
 	
 	load_addr: process(clk, vga_busy, enable)
 	begin
-		vga_we <= '0';
-		if rising_edge(clk) and enable = '1' and vga_busy = '0'
+		if rising_edge(clk) and enable = '1' and vga_busy = '0' 
 		then
-			ticks <= (ticks + 1);
-			vga_we <= '1';
-		elsif ticks >= display_size
-		then	
-			ticks <= 0;
+			if ticks < display_size
+			then
+				ticks <= (ticks + 1);
+				work <= '1';	
+			else
+				ticks <= 0;
+				work <= '0';				
+			end if;
 		end if;
 	end process;
 
